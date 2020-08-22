b64efd807aba4bc1a594860b463868657bf856da
Sergey A Volkov <serg-v@yandex-team.ru>
2020-08-22 19:27:46 +0300

    locust file fix

--- src/main/python/locustfile.py
+++ src/main/python/locustfile.py
@@ -2,10 +2,20 @@ from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 1)
+
     @task
-    def index_page(self):
+    def forecast_1d(self):
+        self.client.get("/api/498817/forecast?duration=P1D")
+
+    @task
+    def forecast_3d(self):
         self.client.get("/api/498817/forecast?duration=P3D")
 
-    # @task
-    # def put(self):
-    #     self.client.put("/", json={"locationId": -1, "timestamp": "2020-07-19T17:12:56+03:00"}, headers={"Secret": "secret"})
+    @task
+    def weather(self):
+        self.client.get("/api/498817/forecast")
+
+
+    @task(0)
+    def put(self):
+        self.client.put("/", json={"locationId": -1, "timestamp": "2020-07-19T17:12:56+03:00"}, headers={"Secret": "secret"})
